75f5a36a72e5330cc47f0325776072e8acd725f9
sahars <hosseinzadeh.sahar.h@gmail.com>
2022-06-06 16:16:05 +0200

    correct the style

--- js-exercise/week1/BookList/ex1-bookList.js
+++ js-exercise/week1/BookList/ex1-bookList.js
@@ -12,70 +12,68 @@
   https: //hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
   */
 
-  function createBookList(books) {
+  const myBooks = [{
+    title: 'The Chatter',
+    author: 'Ethon Kross',
+    alreadyRead: true,
+    imgSrc: "./TheChatter.png"
+  },
+  {
+    title: 'Algorithms to Live By',
+    author: 'Brian Christian',
+    alreadyRead: false,
+    imgSrc: "./Algorithms.png"
+  },
+  {
+    title: 'Selfie',
+    author: 'Will Storr',
+    alreadyRead: false,
+    imgSrc: "./Selfie.png"
+  }
+];
+
+function bookList() {
     const headTitle = document.createElement('h1');
     headTitle.textContent = 'Book List';
     headTitle.style.fontSize = '50px';
     headTitle.style.paddingLeft = '60px';
     document.body.appendChild(headTitle);
-  }
-
-  const ulElement = document.createElement('ul');
-  ulElement.style.display = 'flex';
-  ulElement.style.flexWrap = 'wrap';
-  document.body.appendChild(ulElement);
-
-  for (let i = 0; i < myBooks.length; i++) {
-    const liElement = document.createElement('li');
-    liElement.style.listStyle = 'none';
-    liElement.style.padding = '25px';
-    liElement.style.margin = '20px';
-    liElement.style.borderRadius = '25px';
-    liElement.style.width = '20%';
-
-    ulElement.appendChild(liElement);
-
-    const pElement = document.createElement('p');
-    pElement.textContent = `${myBooks[i].title} - ${myBooks[i].author}`;
-    pElement.style.textAlign = 'center';
-    pElement.style.fontSize = '15px';
-
-    liElement.appendChild(pElement);
-
-    const imgElement = document.createElement('img');
-    imgElement.src = myBooks[i].imgSrc;
-    imgElement.style.width = '100%';
-    imgElement.style.height = '200px';
-    imgElement.style.marginTop = '10px';
-    liElement.appendChild(imgElement);
-
-    if (myBooks[i].alreadyRead === true) {
-      liElement.style.backgroundColor = 'green';
-    } else {
-      liElement.style.backgroundColor = 'red';
+  
+    const ulElement = document.createElement('ul');
+    ulElement.style.display = 'flex';
+    ulElement.style.flexWrap = 'wrap';
+    document.body.appendChild(ulElement);
+  
+    for (let i = 0; i < myBooks.length; i++) {
+      const liElement = document.createElement('li');
+      liElement.style.listStyle = 'none';
+      liElement.style.padding = '35px';
+      liElement.style.margin = '20px';
+      liElement.style.borderRadius = '25px';
+      liElement.style.width = '25%';
+  
+      ulElement.appendChild(liElement);
+  
+      const pElement = document.createElement('p');
+      pElement.textContent = `${myBooks[i].title} - ${myBooks[i].author}`;
+      pElement.style.textAlign = 'center';
+      pElement.style.fontSize = '18px';
+  
+      liElement.appendChild(pElement);
+  
+      const imgElement = document.createElement('img');
+      imgElement.src = myBooks[i].imgSrc;
+      imgElement.style.width = '100%';
+      imgElement.style.height = '350px';
+      imgElement.style.marginTop = '10px';
+      liElement.appendChild(imgElement);
+  
+      if (myBooks[i].alreadyRead === true) {
+        liElement.style.backgroundColor = 'green';
+      } else {
+        liElement.style.backgroundColor = 'red';
+      }
     }
   }
   
-  const books = [{
-      title: 'The Chatter',
-      author: 'Ethon Kross',
-      alreadyRead: true,
-      imgSrc: "/Users/sahar/Desktop/Javascript2/Browsers-DOM/js-exercise/week1/BookList/TheChatter.png"
-    },
-    {
-      title: 'Algorithms to Live By',
-      author: 'Brian Christian',
-      alreadyRead: false,
-      imgSrc: "/Users/sahar/Desktop/Javascript2/Browsers-DOM/js-exercise/week1/BookList/Algorithms.png"
-    },
-    {
-      title: 'Selfie',
-      author: 'Will Storr',
-      alreadyRead: false,
-      imgSrc: "/Users/sahar/Desktop/Javascript2/Browsers-DOM/js-exercise/week1/BookList/Selfie.png"
-    }
-  ];
-  
-  let ulElement = createBookList(books);
-  
-  document.querySelector("#bookList").appendChild(ulElement);
+  bookList();
